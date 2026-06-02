04fd6b8b550f32f26683db90886c1c90f170bcbb
Hope Spencer <61034586+JamieSpencerBSWD@users.noreply.github.com>
2026-06-01 20:15:37 -0500

    Create aboutContent.js

---
+++ src/data/aboutContent.js
@@ -0,0 +1,10 @@
+export const about = {
+    title_one: "I like figuring out",
+    title_two: "how things work",
+
+    about_one:"",
+    about_two:"",
+    about_three:"",
+    about_four:"",
+    
+}
