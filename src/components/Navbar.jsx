dd9e6b7eddb3c1f2c575de897b007f53ac6ceb78
Hope Spencer <61034586+JamieSpencerBSWD@users.noreply.github.com>
2026-06-03 11:26:48 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+import ClearIcon from '@mui/icons-material/Clear';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <nav id="navbar">
         <a href="#hero" className="nav-logo" onClick={() => setIsOpen(false)}>JS<span>.</span></a>
-        <button className="nav-toggle" id="navToggle" aria-label="Toggle navigation" onClick={() => {setIsOpen(!isOpen)}}>☰</button>
+        <button className="nav-toggle" id="navToggle" aria-label="Toggle navigation" onClick={() => {setIsOpen(!isOpen)}}>
+          {!isOpen? 
+          <MenuIcon/> : 
+          <ClearIcon/>
+          }</button>
         <ul className={`nav-links ${isOpen ? "open" : ""}`} id="navLinks" >
             <li><a href="#about" onClick={() => setIsOpen(false)}>About</a></li>
             <li><a href="#skills" onClick={() => setIsOpen(false)}>Skills</a></li>
